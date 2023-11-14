a7fab882a799c3f8ddc0f6e17351ebef8d9cfb42
JM-world <136799243+JM-world@users.noreply.github.com>
2023-11-14 12:29:43 +0900

    Time: 54 ms (5.06%), Space: 16.4 MB (17.28%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,9 +1,10 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         for _ in range(len(s)):
+            if s == '':
+                return True
+            elif '()' not in s and '{}' not in s and '[]' not in s:
+                return False
             s = s.replace('()', '')
             s = s.replace('{}', '')
             s = s.replace('[]', '')
-            if s == '':
-                return True
-        return False
