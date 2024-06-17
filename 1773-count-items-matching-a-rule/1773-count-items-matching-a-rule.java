5c070641bcc1ff07203964a0e7eedba639f88977
JM-world <136799243+JM-world@users.noreply.github.com>
2024-06-17 21:54:14 +0900

    Time: 4 ms (88.81%), Space: 50.8 MB (9.87%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
@@ -3,8 +3,12 @@ class Solution {
         int i = 0;
         int sum = 0;
         switch (ruleKey) {
-            case "color" -> i = 1;
-            case "name" -> i = 2;
+            case "color":
+                i = 1;
+                break;
+            case "name" :
+                i = 2;
+                break;
         }
         
         for (List<String> item : items) {
